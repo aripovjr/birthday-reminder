7bb4af4bccf983cdbf95b03fa7936e5d73e85fc9
Jasurbek Aripov <68148826+aripovjr@users.noreply.github.com>
2021-12-23 03:38:27 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        id: 1,
+        age: 22,
+        name: 'Murad Rakhmatullaev',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg' 
+    },
+    {
+        id: 2,
+        age: 20,
+        name: 'Talib Rashidkhanov',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg' 
+    },
+    {
+        id: 3,
+        age: 21,
+        name: 'Bilal Taxirov',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg' 
+    },
+    {
+        id: 4,
+        age: 20,
+        name: 'Jasur Aripov',
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg' 
+    },
+]
